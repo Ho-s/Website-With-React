68870f197c44e968ad98ee8c7621786721b5abf2
unknown <kjcoco13@gmail.com>
2020-11-26 00:14:12 +0900

    20201126

--- Gallery.jsx
+++ Gallery.jsx
@@ -1,53 +1,62 @@
-import React, { PureComponent } from 'react';
+import React, { createRef, PureComponent } from 'react';
 
 class Gallery extends PureComponent {
+
+    state={
+        deg:0,
+    }
+
+    carousel = createRef()
+
+    clickLeft = () => {
+        this.setState((prevState)=>{
+            return{
+                deg:prevState.deg+10,
+            }
+        })
+        this.carousel.current.style.transform = `rotateY(${this.state.deg}deg)`
+    }
+
+    clickRight=()=>{
+        this.setState((prevState)=>{
+            return{
+                deg:prevState.deg-10,
+            }
+        })
+        this.carousel.current.style.transform = `rotateY(${this.state.deg}deg)`
+    }
+    
+
+    makeCarouselCell=()=>{
+        const cellCount=36
+        const degPerCell=360/cellCount
+        const className=this.state.deg===degPerCell-10 ? 'choice' : 'carousel_cell'
+
+        return(
+            <>
+                {Array(cellCount).fill().map((i)=>{
+                    <div
+                    style={{
+                        backgroundImage: `url('./MYBOX/_${i}.jpg')`,
+                        transform: `rotateY(${degPerCell*(i-1)}deg) translateZ(500px)`
+                    }}
+                    className={className}>
+                    </div>
+                })}
+            </>
+        )
+    }
+
     render() {
-
-
         return (
             <>
-                <div style={{float:'left',width:'100%',height:'100%',backgroundColor:'black'}}>
+                <div style={{ float: 'left', width: '100%', height: '100%', backgroundColor: 'black' }}>
                     <div className='scene'>
-                        <div className='carousel'>
-                            <div style={{backgroundImage:"url('./MYBOX/(1).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (2).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (3).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (4).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (5).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (6).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (7).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (8).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (9).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/(1).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (2).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (3).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (4).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (5).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (6).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (7).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (8).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (9).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/(1).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (2).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (3).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (4).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (5).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (6).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (7).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (8).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (9).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (7).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (8).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (9).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (7).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (8).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (9).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (7).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (8).jpg')"}} className='carousel_cell' ></div>
-                            <div style={{backgroundImage:"url('./MYBOX/ (9).jpg')"}} className='carousel_cell' ></div>
+                        <div ref={this.carousel} className='carousel'>
+                            <makeCarouselCell/>
                         </div>
-                        <button onClick={this.clickLeft}>L</button>
-                        <button onClick={this.clickRight}>R</button>
+                        <button className='button-left' onClick={this.clickLeft}>L</button>
+                        <button className='button-right' onClick={this.clickRight}>R</button>
                     </div>
                 </div>
             </>
